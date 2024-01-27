a28d5900cadebded7699b10cde0305986c077ac2
DevWeb13 <giuliano.loic@sfr.fr>
2024-01-27 12:25:39 +0100

    Playroom multiplayer state

--- src/hooks/useGameEngine.jsx
+++ src/hooks/useGameEngine.jsx
@@ -1,24 +1,51 @@
 // src/hooks/useGameEngine.jsx
 
+import { useMultiplayerState, usePlayersList } from 'playroomkit';
 import React from 'react';
 
-const GameEngineContent = React.createContext({
-  gameEngine: null,
-  setGameEngine: () => {},
-});
+const GameEngineContent = React.createContext({});
 
 const TIME_PHASE_CARDS = 10;
 const TIME_PHASE_PLAYER_CHOICE = 10;
 const TIME_PHASE_PLAYER_ACTION = 3;
 export const NB_ROUNDS = 3;
+const NB_GEMS = 3;
 const CARDS_PER_PLAYER = 4;
 
 export const GameEngineProvider = ({ children }) => {
-  const [gameEngine, setGameEngine] = React.useState(null);
+  // GAME STATE
+  const [timer, setTimer] = useMultiplayerState('timer', 0);
+  const [round, setRound] = useMultiplayerState('round', 1);
+  const [phase, setPhase] = useMultiplayerState('phase', 'lobby');
+  const [playerTurn, setPlayerTurn] = useMultiplayerState('playerTurn', 0);
+  const [playerStart, setPlayerStart] = useMultiplayerState('playerStart', 0);
+  const [deck, setDeck] = useMultiplayerState('deck', []);
+  const [gems, setGems] = useMultiplayerState('gems', NB_GEMS);
+  const [actionsSuccess, setActionsSuccess] = useMultiplayerState(
+    'actionsSuccess',
+    true
+  );
+
+  const players = usePlayersList(true);
+  players.sort((a, b) => a.id.localeCompare(b.id)); // sort players by id
+
+  const gameState = {
+    timer,
+    round,
+    phase,
+    playerTurn,
+    playerStart,
+    players,
+    gems,
+    deck,
+    actionsSuccess,
+  };
 
   return (
     <GameEngineContent.Provider
-      value={{ gameEngine, setGameEngine: React.useState }}>
+      value={{
+        ...gameState,
+      }}>
       {children}
     </GameEngineContent.Provider>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { GameEngineProvider } from './hooks/useGameEngine';
+import { insertCoin } from 'playroomkit';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <GameEngineProvider>
-        <App />
-      </GameEngineProvider>
-    </React.StrictMode>
-  );
+  insertCoin().then(() => {
+    ReactDOM.createRoot(rootElement).render(
+      <React.StrictMode>
+        <GameEngineProvider>
+          <App />
+        </GameEngineProvider>
+      </React.StrictMode>
+    );
+  });
 }
